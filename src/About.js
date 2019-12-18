a1e3bcb8595132c02019137443ca186e8f75baa6
Mike Leisz <clickbecause@clickbecause.lan1>
2019-12-17 21:18:25 -0800

    check mobile before you push mike

--- src/About.js
+++ src/About.js
@@ -7,14 +7,14 @@ function About({ setClose, close }) {
     <Window title={'about'} close={close} setClose={setClose}>
       <AboutText>
         <p>I'm Mike Leisz, a Los Angeles based artist, educator, and creative developer.</p>
-        <p>With 10+ years experience in digital media, I provide technical direction, creative strategy, software development, and motion design for clients like Getty, Red Bull Music, The City of Los Angeles, and DJ Madeon.</p>
+        <p>With 10+ years experience in digital media, I provide technical direction, creative strategy, software development, and motion design for clients like Getty, Red Bull, The City of Los Angeles, and DJ Madeon.</p>
         <p>I currently teach at CalArts in the Music Technology department.</p>
         <br></br>
-        <p>If you'd like to hire me or just say hi: 
+        <p>Hire me or just say hi: 
         <a href="mailto:clickbecause@gmail.com?subject=Hi Mike">
           clickbecause@gmail.com
         </a></p>
-        <p>If you'd like to see some of my audio reactive visual work: 
+        <p>My audio reactive visual work: 
         <a href="https://www.instagram.com/clickbecause/" target="_blank">
           @clickbecause
         </a></p>
